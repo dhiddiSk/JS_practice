090e55309a4e84efadffb21829b798ec11424b36
saikrishna <saikrishna.4369@gmail.com>
2023-07-17 22:53:45 +0200

    eigth challenge solution

---
+++ eigthChallenge.js
@@ -0,0 +1,53 @@
+/*
+Coding Challenge #4
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays �
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! Here is how to
+solve it:
+4.1. First, you will need to add up all values in the array. To do the addition,
+start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop. In each iteration, add the current value to the
+'sum' variable. This way, by the end of the loop, you have all values
+added together
+4.2. To calculate the average, divide the sum you calculated before by the
+length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+GOOD LUCK � 
+*/
+
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+const calcTip = function(bills){
+	for(const item of bills){
+		let tip = (((item > 50) && (item <300))? ((15/100) * item) : ((20/100) * item));
+		let total = (tip + item);		
+		tips.push(tip);
+		totals.push(total);	
+	}	
+}
+calcTip(bills);
+console.log(tips);
+console.log(totals);
+
+const calcAverage = function(arr){
+	let sum = 0;
+	for(const item of arr){
+		sum = sum + item;
+	}
+	let average = sum/(arr.length);
+	return average;	
+}
+console.log(calcAverage(totals));
