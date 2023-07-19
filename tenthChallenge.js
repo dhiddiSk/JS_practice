149344e1269ab1c50dca0b55f9821df91d55add1
saikrishna <saikrishna.4369@gmail.com>
2023-07-19 21:24:44 +0200

    update

--- tenthChallenge.js
+++ tenthChallenge.js
@@ -7,8 +7,6 @@ Your tasks:
 3. Create an array 'allPlayers' containing all players of both teams (22 players)
 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-
-
 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
 
@@ -56,3 +54,10 @@ const game = {
  	}
  }
  printgoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+ 
+ 
+ const teamWins = function(){
+ 	(game.odds.team1 < game.odds.team2) && console.log(`team1 is more likely to win than team2`);
+ }
+ teamWins();
+ 
